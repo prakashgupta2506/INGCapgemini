2250cdc4f3a70993715357a81d28eb5b9993068b
prakashgupta2506 <prakashgupta2506@gmail.com>
2017-06-11 18:47:46 +0530

    reporting

--- src/test/java/com/ingcapgemini/wm/story/leave/Jenkins.java
+++ src/test/java/com/ingcapgemini/wm/story/leave/Jenkins.java
@@ -2,12 +2,14 @@ package com.ingcapgemini.wm.story.leave;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 
 import com.ingcapgemini.wm.common.GlobalStepDefinition;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
 
 public class Jenkins {
 	private static final Logger LOGGER=LogManager.getLogger(GlobalStepDefinition.class);
@@ -22,6 +24,7 @@ public class Jenkins {
 	public void user_clicks_on_link(int arg1) throws Throwable {
 		LOGGER.info("Inside user_clicks_on_link "+arg1);
 	    // Write code here that turns the phrase above into concrete actions
+		Assert.assertTrue("fail",false);
 	  
 	}
 
